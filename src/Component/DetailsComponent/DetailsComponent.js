bf1ca796c07b6bcc43a9abbf3d07bf165cffdc22
JaznanOfficial <jaznanofficial@gmail.com>
2021-09-26 14:46:29 +0600

    design complete

--- src/Component/DetailsComponent/DetailsComponent.js
+++ src/Component/DetailsComponent/DetailsComponent.js
@@ -5,7 +5,7 @@ import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 
 const DetailsComponent = (props) => {
-    const { name, country, creation, dob, salary, img } = props.data || {}
+    const { name, country, creation, dob, salary, img, id } = props.data || {}
     return (
         <div className="card-parent-div">
             <div>
@@ -13,10 +13,12 @@ const DetailsComponent = (props) => {
             </div>
             <div className='details-div'>
                 <h2>{name}</h2>
+                <h4 className='id'>ID: {id}</h4>
                 <h5>Country: {country}</h5>
                 <h5>Creation: {creation}</h5>
                 <h5>DOB: {dob}</h5>
                 <h4>Salary: {salary}</h4>
+                
                 <button><FontAwesomeIcon icon={faShoppingCart} /> Add to Summary</button>
             </div>
         </div>
